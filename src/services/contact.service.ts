18bb4e7849654596ff0a8534f54ea70839f4897f
Joel Silva <joelpitra44@gmail.com>
2025-10-23 13:00:34 +0100

    feat: adicionar a condição caso não seja preenchido o campo da empresa nos emails

--- src/services/contact.service.ts
+++ src/services/contact.service.ts
@@ -18,6 +18,7 @@ export class ContactService {
 
     contact.name = input.name;
     contact.email = input.email;
+    contact.enterprise = input.enterprise ?? "";
     contact.message = input.message;
     contact.phone = input.phone;
 
@@ -39,41 +40,49 @@ export class ContactService {
         to: ["joelpitra44@gmail.com"],
         subject: "Novo contato recebido pelo site",
         html: `
-    <table width="100%" cellpadding="0" cellspacing="0" style="background-color: #f4f6f8; padding: 0px 0;">
-      <tr>
-        <td align="center">
-          <table width="600" cellpadding="0" cellspacing="0" style="background-color: #ffffff; border-radius: 8px; overflow: hidden; font-family: Arial, Helvetica, sans-serif;">
-            <tr style="background-color: #ffffff">
-              <td style="text-align: center;">
-                <img src="https://globalsc.ao/logo-email.png" alt="Global Services Corporation" style="width: 100%; object-fit: contain" />
-              </td>
-            </tr>
-            <tr>
-              <td style="padding: 32px;">
-                <p style="color: #333; font-size: 15px;">Um novo visitante preencheu o formulário no site da <strong>Global Services Corporation</strong>.</p>
-                <hr style="border: none; border-top: 1px solid #ddd; margin: 24px 0;" />
-                <p style="margin: 8px 0;"><strong>Nome:</strong> ${
-                  contact.name
-                }</p>
-                <p style="margin: 8px 0;"><strong>Email:</strong> ${
-                  contact.email
-                }</p>
-                <p style="margin: 8px 0;"><strong>Telefone:</strong> ${
-                  contact.phone || "—"
-                }</p>
-                <p style="margin-top: 16px;"><strong>Mensagem:</strong></p>
-                <div style="background-color: #f0f4f9; padding: 12px 16px; border-left: 4px solid #006eebff; color: #333; border-radius: 4px;">
-                  ${contact.message}
-                </div>
-                <hr style="border: none; border-top: 1px solid #ddd; margin: 32px 0;" />
-                <p style="font-size: 12px; color: #777;">Este é um e-mail automático do sistema de contatos do site.</p>
-              </td>
-            </tr>
-          </table>
-        </td>
-      </tr>
-    </table>
-  `,
+      <table width="100%" cellpadding="0" cellspacing="0" style="background-color: #f4f6f8; padding: 0;">
+        <tr>
+          <td align="center">
+            <table width="600" cellpadding="0" cellspacing="0" style="background-color: #ffffff; border-radius: 8px; overflow: hidden; font-family: Arial, Helvetica, sans-serif;">
+              <tr>
+                <td style="text-align: center;">
+                  <img src="https://globalsc.ao/logo-email.png" alt="Global Services Corporation" style="width: 100%; object-fit: contain;" />
+                </td>
+              </tr>
+              <tr>
+                <td style="padding: 32px;">
+                  <p style="color: #333; font-size: 15px;">Um novo visitante preencheu o formulário no site da <strong>Global Services Corporation</strong>.</p>
+                  <hr style="border: none; border-top: 1px solid #ddd; margin: 24px 0;" />
+                  
+                  <p style="margin: 8px 0;"><strong>Nome:</strong> ${
+                    contact.name
+                  }</p>
+                  <p style="margin: 8px 0;"><strong>Email:</strong> ${
+                    contact.email
+                  }</p>
+                  ${
+                    contact.enterprise
+                      ? `<p style="margin: 8px 0;"><strong>Empresa:</strong> ${contact.enterprise}</p>`
+                      : ""
+                  }
+                  <p style="margin: 8px 0;"><strong>Telefone:</strong> ${
+                    contact.phone || "—"
+                  }</p>
+
+                  <p style="margin-top: 16px;"><strong>Mensagem:</strong></p>
+                  <div style="background-color: #f0f4f9; padding: 12px 16px; border-left: 4px solid #006eeb; color: #333; border-radius: 4px;">
+                    ${contact.message}
+                  </div>
+
+                  <hr style="border: none; border-top: 1px solid #ddd; margin: 32px 0;" />
+                  <p style="font-size: 12px; color: #777;">Este é um e-mail automático do sistema de contatos do site.</p>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+      `,
       });
 
       await this.resend.emails.send({
@@ -81,34 +90,41 @@ export class ContactService {
         to: [contact.email],
         subject: "Recebemos a sua mensagem",
         html: `
-    <table width="100%" cellpadding="0" cellspacing="0" style="background-color: #f4f6f8; padding: 0px 0;">
-      <tr>
-        <td align="center">
-          <table width="600" cellpadding="0" cellspacing="0" style="background-color: #ffffff; border-radius: 8px; overflow: hidden; font-family: Arial, Helvetica, sans-serif;">
-            <tr style="background-color: #0077ffff">
-              <td style=" text-align: center;">
-                <img src="https://globalsc.ao/logo-email.png" alt="Global Services Corporation" style=" width: 100%; object-fit: contain" />
-              </td>
-            </tr>
-            <tr>
-              <td style="padding: 32px;">
-                <p style="color: #333; font-size: 15px;">Olá, Sr.(a) <strong>${contact.name}</strong>,</p>
-                <p style="color: #333; font-size: 15px;">Agradecemos por entrar em contacto com a <strong>Global Services Corporation</strong>.</p>
-                <p style="color: #333; font-size: 15px;">Confirmamos o recebimento da sua mensagem. A nossa equipa irá analisá-la e retornaremos em breve através do e-mail ou telefone fornecido.</p>
-                <p style="margin-top: 32px; color: #333; font-size: 15px;">
-                  <em>Atenciosamente,</em><br/>
-                  <strong>Global Services Corporation</strong><br/>
-                  <a href="https://globalsc.ao" style="color: #0077ffff; text-decoration: none;">www.globalsc.ao</a>
-                </p>
-                <hr style="border: none; border-top: 1px solid #ddd; margin: 32px 0;" />
-                <p style="font-size: 12px; color: #777;">Este é um e-mail automático. Por favor, não responda diretamente.</p>
-              </td>
-            </tr>
-          </table>
-        </td>
-      </tr>
-    </table>
-  `,
+      <table width="100%" cellpadding="0" cellspacing="0" style="background-color: #f4f6f8; padding: 0;">
+        <tr>
+          <td align="center">
+            <table width="600" cellpadding="0" cellspacing="0" style="background-color: #ffffff; border-radius: 8px; overflow: hidden; font-family: Arial, Helvetica, sans-serif;">
+              <tr style="background-color: #0077ff;">
+                <td style="text-align: center;">
+                  <img src="https://globalsc.ao/logo-email.png" alt="Global Services Corporation" style="width: 100%; object-fit: contain;" />
+                </td>
+              </tr>
+              <tr>
+                <td style="padding: 32px;">
+                  <p style="color: #333; font-size: 15px;">Olá, Sr.(a) <strong>${
+                    contact.name
+                  }</strong>${
+          contact.enterprise ? ` da <strong>${contact.enterprise}</strong>` : ""
+        },</p>
+                  
+                  <p style="color: #333; font-size: 15px;">Agradecemos por entrar em contacto com a <strong>Global Services Corporation</strong>.</p>
+                  <p style="color: #333; font-size: 15px;">Confirmamos o recebimento da sua mensagem. A nossa equipa irá analisá-la e retornaremos em breve através do e-mail ou telefone fornecido.</p>
+                  
+                  <p style="margin-top: 32px; color: #333; font-size: 15px;">
+                    <em>Atenciosamente,</em><br/>
+                    <strong>Global Services Corporation</strong><br/>
+                    <a href="https://globalsc.ao" style="color: #0077ff; text-decoration: none;">www.globalsc.ao</a>
+                  </p>
+
+                  <hr style="border: none; border-top: 1px solid #ddd; margin: 32px 0;" />
+                  <p style="font-size: 12px; color: #777;">Este é um e-mail automático. Por favor, não responda diretamente.</p>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+      `,
       });
     } catch (error) {
       console.error("Erro ao enviar email via Resend:", error);
